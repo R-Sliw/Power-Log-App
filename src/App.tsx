54e2e9ba2f2a8d4fdb1cb7e6deb1a431dcd03b4b
R-Sliw <robert.sliwinski90@gmail.com>
2022-03-30 10:43:42 +0200

    added inputs

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      PowerApp
-    </div>
+    <main className="App">
+      <label htmlFor="date">Dzień treningu: </label>
+      <input type="date" name="date" id="date" />
+      <button> OK </button>
+      <label htmlFor="workout">Rodzaj treningu: </label>
+      <select name="workout" id="workout">
+        <option value="">Klatka</option>
+        <option value="">Nogi</option>
+        <option value="">Barki</option>
+      </select>
+      <button> OK </button>
+       <label htmlFor="workout">Nr serii: </label>
+      <input type="number" name="" id="" />
+      <button> OK </button>
+       <label htmlFor="workout">Ilość potworzeń: </label>
+      <input type="number" name="" id="" />
+      <button> OK </button>
+    </main>
   );
 }
 
